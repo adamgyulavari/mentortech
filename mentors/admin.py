5b50bce1b67f294361cd255b78b7578c7f3f88f0
Adam Gyulavari <gyulavari.adam@gmail.com>
2016-01-23 18:51:30 +0100

    modify email and description field in mentor and

--- mentors/admin.py
+++ mentors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Mentor
+from .models import Opinion
 
 # Register your models here.
+admin.site.register(Mentor)
+admin.site.register(Opinion)
--- mentors/models.py
+++ mentors/models.py
@@ -5,9 +5,16 @@ from django.db import models
 class Mentor(models.Model):
     first_name  = models.CharField(max_length=50)
     last_name   = models.CharField(max_length=50)
-    email       = models.CharField(max_length=100)
-    description = models.TextField()
+    email       = models.EmailField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name + ' (' + self.email + ')'
 
 class Opinion(models.Model):
+    mentor      = models.ForeignKey(Mentor)
     name        = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name + ' (' + self.mentor.email + ')'
